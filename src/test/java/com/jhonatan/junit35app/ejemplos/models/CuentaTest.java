dbbdfb8824d6ba01194c2c295062e38a1ce9b59c
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-10-14 01:14:33 -0500

    clase cuenta: importando una libreria de para los metodos

--- src/test/java/com/jhonatan/junit35app/ejemplos/models/CuentaTest.java
+++ src/test/java/com/jhonatan/junit35app/ejemplos/models/CuentaTest.java
@@ -2,24 +2,25 @@ package com.jhonatan.junit35app.ejemplos.models;
 
 import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
-//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CuentaTest {
 
     //creamos un metodo test
     @Test
     void testNombreCuenta() {
-        Cuenta c = new Cuenta("Andrez",new BigDecimal("1000.12345"));
+        Cuenta c = new Cuenta("Andrez", new BigDecimal("1000.12345"));
         //c.setPersona("Andrez");
 
         String esperado = "Andrez";
         String real = c.getPersona();
-        
+
         //hacemos un test osea comparamos
-        Assertions.assertEquals(esperado, real);
+        assertEquals(esperado, real);
+        
+        //otro metodo 
+        assertTrue(real.equals("Andrez"));
+        
     }
-    
-    
-    
+
 }
